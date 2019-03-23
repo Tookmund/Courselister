1858d46f39ee27f6fdd32593483ac8eec85c6bd5
Jacob Adams <tookmund@gmail.com>
2019-03-23 00:52:14 -0400

    Clean up script

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 var db = null;
 
 var xhr = new XMLHttpRequest();
-xhr.open('GET', '202010.sqlite', true);
+xhr.open('GET', '202010.db', true);
 xhr.responseType = 'arraybuffer';
 
 xhr.onload = function(e) {
   var uInt8Array = new Uint8Array(this.response);
   db = new SQL.Database(uInt8Array);
-  alert("LOADED");
-  console.log(db.exec("SELECT * from courses LIMIT 1"));
 };
 xhr.send();
 console.log('begin');
