13ca7c9df73debe715e7fba8fd7866b8b0a363b3
Jacob Adams <tookmund@gmail.com>
2019-03-22 23:00:57 -0400

    Add scraper

---
+++ scraper.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python3
+
+import os
+import re
+import bs4
+import requests
+import sqlite3
+
+cs = requests.get("https://courselist.wm.edu/courselist/")
+if cs.status_code != 200:
+    print("Course List", cs.status_code)
+    os.exit(1)
+
+csp = bs4.BeautifulSoup(cs.text, 'lxml')
+
+
+def selectvalues(select):
+    vals = []
+    for opt in select.children:
+        if isinstance(opt, bs4.element.Tag):
+            v = opt['value']
+            if v != '0':
+                vals.append(opt['value'])
+    return vals
+
+terms = selectvalues(csp.find(id='term_code'))
+subjs = selectvalues(csp.find(id='term_subj'))
+
+def parsetable(c, table):
+    pass
+
+#for term in terms[:1]:
+term = terms[2]
+db = sqlite3.connect(term+'.db')
+c = db.cursor()
+# CRN int
+# coll 0 = none, 100, 150, 200, 30D, 30G, 30C etc.
+# TODO Multiple COLL attributes
+c.execute('''
+        CREATE TABLE IF NOT EXISTS courses
+        (
+        CRN int,
+        ID  text,
+        Attr text,
+        COLL text,
+        Title text,
+        InstF text,
+        InstL text,
+        credits int,
+        days text,
+        time text,
+        enrolled int
+        seats int,
+        status bool
+        )
+        ''')
+
+
+ATTRS = ['CSI', 'NQR', 'ALV']
+
+
+coll = re.compile(r'C\d{2}.')
+def parserow(row, c):
+    print(len(row))
+    course = [None for i in range(12)]
+    course[0] = row[0].a.string
+    course[1] = row[1].string
+    print(row[2].string)
+    attr = row[2].string.split(',')
+    if not isinstance(attr, list):
+        attr = [attr]
+    for item in attr:
+        if item in ATTRS:
+            course[2] = item
+        match = coll.search(item)
+        if match:
+            course[3] = item[1:]
+
+    course[4] = row[4].string
+    fl = row[5].string.split(',')
+    course[5] = fl[1]
+    course[6] = fl[0]
+    course[7] = row[6].string
+    dt = row[7].string.split(":")
+    course[8] = dt[0]
+    course[9] = dt[1]
+    # row[8] is projected
+    course[10] = row[9].string
+    course[11] = row[10].string
+    if row[11].string == "OPEN":
+        course[12] = True
+    else:
+        course[12] = False
+
+    v = "{}, " *11+"{}"
+    c.execute("INSERT INTO courses VALUES ("+v+")".format(*row))
+    c.commit()
+
+for subj in subjs[:1]:
+    r = requests.get("https://courselist.wm.edu/courselist/courseinfo/searchresults?term_code="+term+"&term_subj="+subj+"&attr=0&attr2=0&levl=0&status=0&ptrm=0&search=Search")
+    if r.status_code != 200:
+        print(term_code, subj, r.status_code)
+        os.exit(2)
+    parse = bs4.BeautifulSoup(r.text, 'lxml')
+    t = parse.find('table')
+    rowsize = 11
+    row = []
+    i = 0
+    for data in t.find_all('td'):
+        if i == rowsize:
+            parserow(row, c)
+            row = []
+            i = 0
+            pass
+        row.append(data)
+        i += 1
